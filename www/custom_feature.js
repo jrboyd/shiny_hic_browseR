fe202be8add2a5291b866977b96913dd1f905b7e
jrboyd <jrboyd@med.uvm.edu>
2017-11-13 15:56:08 -0500

    polygon feature works.

--- www/custom_feature.js
+++ www/custom_feature.js
@@ -1,7 +1,9 @@
 var custom_feature = function (div) {
   
   var board = tnt.board().from(-1).to(5).min(-8).max(10).width(500).zoom_out(10).zoom_in(1);
-  
+
+
+
   // Axis track
   var axis_track = tnt.board.track()
   .height(0)
@@ -10,14 +12,24 @@ var custom_feature = function (div) {
            .orientation("top")
   );
   
+  /*
+  board.pane
+  .append("text")
+  .text("debug");
+  */
+  
   // arrow feature
-  var arrow_feature = tnt.board.track.feature();
+  var polygon_feature = tnt.board.track.feature();
   
   // Create
-  arrow_feature.create (function (elems) {
-    var xScale = arrow_feature.scale();
+  polygon_feature.create (function (elems) {
+    
    
     var track = this;
+    var xScale = polygon_feature.scale();
+    var xresize = function(size){
+      return board.width() * (size / (board.to() - board.from()));
+    };
     var y = track.height();
     //var x = track.width();
  
@@ -28,112 +40,61 @@ var custom_feature = function (div) {
       //return "translate(" + (0 + d.mid) + "," + (y - (0.2 + d.dist)/(1 + 2)*y) + ")";
     });
     g
-    .append("line")
-    .attr("x1", 0)
-    .attr("y1", 0)
-    .attr("x2", 0)
-    .attr("y2", function(d){
-      return 0;
-      //return -(1 + d.dist)/(1 + 2)*y;
-    })
-    .attr("stroke", function (d) {
-      return d.color;
-    });
-    
-    g
-    .append("line")
-    .attr("x1", -10)
-    .attr("y1", function(d){
-      return 0;
-      //return -(1 + d.dist)/(1 + 2)*y/2;
-    })
-    .attr("x2", +10)
-    .attr("y2", function(d){
-      return 0;
-      //return -(1 + d.dist)/(1 + 2)*y/2;
-    })
-    .attr("stroke", function (d) {
-      return d.color;
-    });
-    
-    g
-    .append("polygon")
-    .attr("points", "-25,0 0,25 25,0 0,-25")
-    .attr("fill", function(d){
-      return d.color;
-    });
-    
-    /*
-    g
     .append("polygon")
     .attr("points", function(d){
-      return (-track.scale(d.size) + ",0 0,5 " + track.scale(d.size) + ",0 0,-5");
+      return -xresize(d.size) + ",0 0,25 " + xresize(d.size) + ",0 0,-25";
+      //return "-25,0 0,25 25,0 0,-25";
     })
     .attr("fill", function(d){
       return d.color;
     });
-    */
-    
-    
     /*
-    function (d) {
-      return 0;
-      //return xScale(d.start(d, i));
-      //return xScale(d.mid - d.w / 2);
-    })
-    .attr("y", -y*0.4)
-    .attr("width", function (d) {
-      return d.w;
-    })
-    .attr("height", y*0.2)
-    .attr("stroke", function (d) {
-      return d.color;
-    })
-    .attr("fill", function (d) {
-      return d.color;
-    });
+    g
+    .append("text")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("style", "fill:blue")
+    .text("test");
     */
-    
-
-
 
   });
   
-  /*
-     arrow_feature.distribute (function (blocks) {
-        var xScale = arrow_feature.scale();
-    	blocks
-    	    .select("rect")
-    	    .attr("width", function (d) {
-                return (xScale(d.end) - xScale(d.start));
-    	    });
-    });
-    */
   
   // Move
-  arrow_feature.move (function (arrows) {
-    var xScale = arrow_feature.scale();
+  polygon_feature.move (function (blocks) {
+    var xScale = polygon_feature.scale();
     var track = this;
     var y = track.height();
 
-    arrows.select("g")
-    .attr("transform", function (d) {
-      return "translate(" + xScale(d.mid) + "," + (y - (0.2 + d.dist)/(1 + 2)*y) + ")";
+/*
+    blocks.select("polygon")
+    .attr("points", function(d){
+      return -xresize(d.size) + ",0 0,25 " + xresize(d.size) + ",0 0,-25";
+      //return "-25,0 0,25 25,0 0,-25";
     });
+  */  
+       blocks.select("polygon")
+    .attr("points", function(d){
+      return xScale(board.from()) + ",0 0,25 " + xScale(board.to()) + ",0 0,-25";
+      //return -board.width()/2 + ",0 0,25 " + board.width()/2 + ",0 0,-25";
+      //return "-25,0 0,25 25,0 0,-25";
+    });
+    
   });
   
+
   
   
   // Data track
-  var arrow_track = tnt.board.track()
+  var polygon_track = tnt.board.track()
   .height(60)
   .color("white")
-  .display(arrow_feature)
+  .display(polygon_feature)
   .data(tnt.board.track.data.sync()
         .retriever (function () {
           return [
             {
-              mid : 1,
+              mid : .5,
               size : 1,
               dist : 0,
               color : "red",
@@ -180,7 +141,7 @@ var custom_feature = function (div) {
   
   
   board
-  .add_track([axis_track, arrow_track]);
+  .add_track([axis_track, polygon_track]);
   
   board(div);
   
